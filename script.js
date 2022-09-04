6290b0019410bb75bbd3a94162fecbc5e2a94505
Takeshita Nao <takeshita@t.nao>
2022-09-04 15:26:32 +0900

    add images

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-window.setTimeout(function(){
+window.setTimeout(function () {
     document.querySelector(".lets_start").style.display = "none";
     console.log("fuck")
 }, 3500);
+
+for (let i = 1; i < 24; i++) {
+    let dl_area = document.createElement("div");
+    dl_area.setAttribute("class", "dl-area");
+    let dl_img = document.createElement("img");
+    dl_img.setAttribute("src", "gallery/IMG_" + String(i) + ".webp");
+    dl_area.appendChild(dl_img);
+    let a = document.createElement("a");
+    a.setAttribute("href", "gallery/IMG_" + String(i) + ".JPG");
+    a.setAttribute("download", "");
+    let btn = document.createElement("button");
+    btn.textContent = "ダウンロード";
+    a.appendChild(btn);
+    dl_area.appendChild(a)
+    document.querySelector(".gallery").appendChild(dl_area)
+}
