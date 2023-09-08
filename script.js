0fa01763b0e7b8e11f22798445b3e546fa5ac668
257cc8ae <kny70dr@gmail.com>
2023-09-08 22:52:38 +0900

    changed

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-window.setTimeout(function () {
-    document.querySelector(".lets_start").style.display = "none";
-    console.log("fuck")
-}, 3500);
-
-for (let i = 1; i < 27; i++) {
+for (let i = 1; i < 30; i++) {
     let dl_area = document.createElement("div");
     dl_area.setAttribute("class", "dl-area");
     let dl_img = document.createElement("img");
     dl_img.setAttribute("src", "gallery/IMG_" + String(i) + ".webp");
+    dl_img.setAttribute("loading", "lazy")
     dl_area.appendChild(dl_img);
     let a = document.createElement("a");
     a.setAttribute("href", "gallery/IMG_" + String(i) + ".JPG");
